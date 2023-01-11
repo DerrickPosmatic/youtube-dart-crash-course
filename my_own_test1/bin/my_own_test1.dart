bbe3671f14a9e48a38177e2cd82e7505551228df
Derrick Lee <derrick@posmatic.com>
2023-01-11 14:29:26 +0900

    Just whatever I created following the tutorial

---
+++ my_own_test1/bin/my_own_test1.dart
@@ -0,0 +1,5 @@
+import 'package:my_own_test1/my_own_test1.dart' as my_own_test1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_own_test1.calculate()}!');
+}
---
+++ my_own_test1/test/my_own_test1_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_own_test1/my_own_test1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
